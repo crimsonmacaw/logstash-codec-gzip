5de0fe5d9dcd58f469ad8424fb9bfc88aa974f06
Robert Bruce <robert.bruce@crimsonmacaw.com>
2018-11-15 11:31:39 +0000

    Added charset support for converting after decompressing or before compressing

--- lib/logstash/codecs/gzip.rb
+++ lib/logstash/codecs/gzip.rb
@@ -23,22 +23,46 @@ class LogStash::Codecs::Gzip < LogStash::Codecs::Base
   # The codec name
   config_name "gzip"
 
-  def register
-  end # def register
+  # The character encoding used in this codec. Examples include "UTF-8" and
+  # "CP1252"
+  #
+  # JSON requires valid UTF-8 strings, but in some cases, software that
+  # emits JSON does so in another encoding (nxlog, for example). In
+  # weird cases like this, you can set the charset setting to the
+  # actual encoding of the text and logstash will convert it for you.
+  #
+  # For nxlog users, you'll want to set this to "CP1252"
+  config :charset, :validate => ::Encoding.name_list, :default => "UTF-8"
+
+  def initialize(params={})
+    super(params)
+    @converter = LogStash::Util::Charset.new(@charset)
+    @converter.logger = @logger
+  end
 
   def decode(data)
-    gz = Zlib::GzipReader.new(StringIO.new(data.to_s))
+    begin
+      gz = Zlib::GzipReader.new(StringIO.new(data.to_s))
 
-    yield LogStash::Event.new({
-      "message" => gz.read
-    })
+      yield LogStash::Event.new({
+        "message" => @converter.convert(gz.read)
+      })
+    rescue Zlib::Error => e
+      @logger.error("Gzip codec: We cannot uncompress the gzip data")
+      raise e
+    end
   end # def decode
 
   # Encode a single event, this returns the raw data to be returned as a String
   def encode_sync(event)
-    gz = Zlib::GzipWriter.new(StringIO.new)
-    gz << event.get("message").to_s
-    gz.close.string
+    begin
+      gz = Zlib::GzipWriter.new(StringIO.new)
+      gz << @converter.convert(event.get("message").to_s)
+      gz.close.string
+    rescue Zlib::Error => e
+      @logger.error("Gzip codec: We cannot compress the gzip data")
+      raise e
+    end
   end # def encode_sync
 
 end # class LogStash::Codecs::Gzip
